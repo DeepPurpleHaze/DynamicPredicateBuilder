9c80e758b36d03fdbc04efeb8543da777489dce7
Pavel Plakhotniuk <pavel.plachotnjuk@gmail.com>
2019-07-16 22:03:43 +0300

    Initial commit, empty project

---
+++ DynamicPredicateBuilder/DynamicPredicateBuilder/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DynamicPredicateBuilder.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DynamicPredicateBuilder/DynamicPredicateBuilder/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DynamicPredicateBuilder.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
